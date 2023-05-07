313b40f8dab442740024cb0e7935384f9d290f30
Kamyar <kammirzazad@gmail.com>
2023-05-07 07:08:53 +0000

    added new 1-fps list, re-orged old lists

--- imagelists/genDownsampleList.py
+++ imagelists/genDownsampleList.py
@@ -1,12 +1,26 @@
 import	sys
 
-rate = int(sys.argv[1])
-maxFrame = 81169
+if len(sys.argv) != 3:
+    print('usage:', sys.argv[0], '[fps]', '[numFrames]')
+    exit(1)
+
+fps = int(sys.argv[1])
+numFrames = int(sys.argv[2])
+
+assert fps < 30, 'baseline fps is 30'
+
+maxIndex = 81169
+startIndex = 32000
 
 indice = []
-for index in range(0,maxFrame+1,rate):
-	indice.append(index)
+for i in range(numFrames):
+    index = int(startIndex + i * (30 / fps))
+    if index > maxIndex:
+        print('there are only ', i, 'frames @', fps, 'fps')
+        break
+    indice.append(index)
 
-with open('AGZ_downsampled_'+str(rate)+'.txt','w') as fh:
-	for index in indice[-200:]:
-		fh.write(str(index).zfill(5)+'.txt\n')
+fn = 'AGZ_' + str(fps) + '_fps_' + str(numFrames) + '.txt'
+with open(fn, 'w') as fh:
+    for index in indice:
+        fh.write(str(index).zfill(5)+'.txt\n')
